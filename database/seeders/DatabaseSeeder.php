fa01b615ae3221ddaadb6d9de8461e575a6584c6
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-25 14:28:38 +0200

    feat: add migration for table pivot project_technology

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypesSeeder::class,
+            TechnologiesSeeder::class,
             ProjectsSeeder::class
         ]);
     }
