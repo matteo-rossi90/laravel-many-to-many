2733768d844be5a500b912aef9473b8250a8a1bb
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-25 15:17:53 +0200

    feat: add relationship many to many in techology and Project models and add ProjectTechnologySeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TypesSeeder::class,
             TechnologiesSeeder::class,
-            ProjectsSeeder::class
+            ProjectsSeeder::class,
+            ProjectTechnologySeeder::class
         ]);
     }
 }
