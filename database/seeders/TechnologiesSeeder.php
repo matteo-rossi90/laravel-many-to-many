da98e3986bde3f673901328d2477732d2ffad613
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-09-25 14:10:15 +0200

    feat: add migration, model and seeder for technologies

---
+++ database/seeders/TechnologiesSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Functions\Helper;
+use App\Models\Technology;
+
+class TechnologiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $data = [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'React',
+            'Node.js',
+            'Express.js',
+            'MongoDB',
+            'Git',
+            'Docker',
+            'AWS'
+        ];
+
+        foreach($data as $technology){
+
+            $new_technology = new Technology();
+            $new_technology->name = $technology;
+            $new_technology->slug = Helper::generateSlug($new_technology->name, Technology::class);
+            $new_technology->save();
+        }
+    }
+}
